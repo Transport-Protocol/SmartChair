c73cf575e3491dcb559d3473c867395410af92d6
hauke.buhr <hauke.buhr@haw-hamburg.de>
2016-10-11 22:05:38 +0200

    added support for commandline parameters and a new thread start routine

--- team_sensor/pi_python/main.py
+++ team_sensor/pi_python/main.py
@@ -1,40 +1,76 @@
-#!/usr/bin/python
+#!/usr/bin/python3
 
-## Requires pika for queue connection
+from optparse import OptionParser
 
-import queue as QueuePackage
+import queue as queue_package
 import Communicators as communicators
 import Threads as own_threads
 import HAL
 
+# Define here all supported sensors.
+sensors = ["pressure"]
+sensor = ["temperature", "location"]
 
-json_queue = QueuePackage.Queue()
+parser = OptionParser()
 
-# Get the communicator and Setup the connection
-communicator = communicators.CommunicatorDummy()
-# communicator = communicators.RabbitMQCommunicator("127.0.0.1", "sg.ex.sensor_values", "sg.rk.sensor_values", json_queue)
-# communicator = communicators.SocketCommunicator("141.22.80.72", 15000)
+parser.add_option("-s", "--specific",
+                  action="store_true", dest="test", default=False,
+                  help="This flag enables test mode, now you can use only specific types of sensors.")
+
+parser.add_option("--rabbit",
+                  action="store_true", dest="rabbit", default=False,
+                  help="This flag causes json strings to send to rabbitMQ.")
+
+parser.add_option("--socket",
+                  action="store_true", dest="socket", default=False,
+                  help="This flag causes json strings to send by socket.")
+
+for entry in sensors:
+    parser.add_option("--" + entry,
+                      action="store_true", dest=entry, default=False,
+                      help="This flag enables " + entry + " sensors in test mode")
+
+for entry in sensor:
+    parser.add_option("--" + entry,
+                      action="store_true", dest=entry, default=False,
+                      help="This flag enables " + entry + " sensor in test mode")
+
+(options, args) = parser.parse_args()
+
+
+# get collect all needed sensor threads
+json_queue = queue_package.Queue()
+threads = []
+if options.test:
+    print("Test mode active!")
+
+    if options.pressure:
+        threads.append(own_threads.SensorEvaluator(1, "SensorEvaluator_pressure", 1, 1, json_queue, HAL.serial_sensors))
+    if options.temperature:
+        threads.append(own_threads.SensorEvaluator(2, "SensorEvaluator_temperature", 2,  30, json_queue, HAL.serial_sensors))
+    if options.location:
+        # threads.append(own_threads.SensorEvaluator(3, "SensorEvaluator_location", 3, 5, json_queue, HAL.serial_sensors))
+        print("location is currently not supported!")
+else:
+    threads.append(own_threads.SensorEvaluator(1, "SensorEvaluator_pressure", 1, 1, json_queue, HAL.serial_sensors))
+    # threads.append(own_threads.SensorEvaluator(2, "SensorEvaluator_temperature", 2,  30, json_queue, HAL.serial_sensors))
+    # threads.append(own_threads.SensorEvaluator(3, "SensorEvaluator_location", 3, 5, json_queue, HAL.serial_sensors))
+
+# get right communication medium
+if options.rabbit:
+    communicator = communicator = communicators.RabbitMQCommunicator("127.0.0.1", "sg.ex.sensor_values", "sg.rk.sensor_values", json_queue)
+elif options.socket:
+    communicator = communicators.SocketCommunicator("141.22.80.72", 15000)
+else:
+    communicator = communicators.CommunicatorDummy()
+
 communicator.setup_connection()
-
-# create threads
-serial_sensors_thread = own_threads.SensorEvaluator(1, "SensorEvaluator_pressure", 1, json_queue, HAL.serial_sensors)
-communicator_thread = own_threads.MQ_Communicator(10, "MQ_Communicator", 10, json_queue, communicator)
+threads.append(own_threads.MQ_Communicator(10, "MQ_Communicator", 10, json_queue, communicator))
 
 # start the communicator thread
-serial_sensors_thread.start()
-communicator_thread.start()
-
-
+for thread in threads:
+    thread.start()
 
 # join all threads
-serial_sensors_thread.join()
-communicator_thread.join()
-
-
-
-
-
-
-
-
-
+for thread in threads:
+    thread.join()
