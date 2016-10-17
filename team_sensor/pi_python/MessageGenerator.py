838d889df78576262075dd2418678d7068b5623f
hauke.buhr <hauke.buhr@haw-hamburg.de>
2016-10-17 21:21:00 +0200

    added support for beacon messages

--- team_sensor/pi_python/MessageGenerator.py
+++ team_sensor/pi_python/MessageGenerator.py
@@ -1,8 +1,7 @@
 import json
 
 
-def pack_to_json(version, sensor_type, id_nr, values):
-
+def pack_to_json(version, timestamp, sensor_type, id_nr, values):
     value_string = '"values": ['
 
     for id_num in id_nr:
@@ -11,7 +10,16 @@ def pack_to_json(version, sensor_type, id_nr, values):
         if id_num < len(id_nr) - 1:
             value_string += ', '
 
-    value_string += "]}"
-    json_msg = '{"version": ' + str(version) + ', "sensortype": "' + sensor_type + '", ' + value_string
+    json_msg = '{"version": ' + str(version) + ', "timestamp": ' + \
+               timestamp + ', "sensortype": "' + sensor_type + '", ' + value_string + "]}"
+
+    return json_msg
+
+
+def pack_location_to_json(version, timestamp, sensor_type, uuid, major, minor, db):
+    beacon_json = '{"uuid" : "' + uuid + '", "major" : "' + major + '", "minor" : "' + minor + '", "dB" : "' + db + '}'
+
+    json_msg = '{"version": ' + str(version) + ', "timestamp": ' + \
+               timestamp + ', "sensortype": "' + sensor_type + '", "values": ' + beacon_json + "]}"
 
     return json_msg
