7a91a32ef80eca9839dbeb47202ef01acec91908
Ralf von der Reith <ralf-von-der-reith@gmx.de>
2016-10-17 11:31:14 +0200

    merged server and client code

--- team_database/app.js
+++ team_database/app.js
@@ -1,5 +1,5 @@
 var express = require('express')
-
+//server stuff
 var app = express()
 
 app.use(express.static('www'));
@@ -13,30 +13,80 @@ var server = app.listen(8000, function () {
 
 })
 
+//database variables
+var influx = require('influx')
+var username = 'root'
+var password = 'root'
+var database = 'clarc_sensor'
 
+var dbClient = influx({host: 'localhost', username: username, password: password, database: database})
+
+// socket connection
+
+//constants:
+const PRESSURE_INTERVAL = 200;
+const TEMPERATURE_INTERVAL = 5000;
 // Loading socket.io
 var io = require('socket.io').listen(server);
 
-// When a client connects, we note it in the console
-io.sockets.on('connection', function (socket) {
-    console.log('A client is connected!');
-	socket.emit('connect');
+io.sockets.on('connection', function(socket) {
+	console.log('[socket.io] Ein neuer Client (Browser) hat sich verbunden.\n');
 
-    var dataIntervalId;
-    // When the server receives a “message” type signal from the client   
-    socket.on('data', function () {
-    	console.log('received "data"');
-    	dataIntervalId = setInterval(function() {
-    		socket.emit('pressure', JSON.stringify(generatePressure()));
-            socket.emit('temperature', JSON.stringify(generateTemperature()));
-    	}, 50);
+	socket.on('data', function(data) {
+
+		var pressureIntervalID = setInterval(function() {
+			socket.emit('pressure', JSON.stringify(getPressure()));
+		}, PRESSURE_INTERVAL);
+		var temperatureIntervalID = setInterval(function() {
+			socket.emit('temperature', JSON.stringify(getTemperature()));
+		}, TEMPERATURE_INTERVAL);
 	});
-    socket.on('stop', function() {
-    		clearInterval(dataIntervalId);
-    		socket.emit('stop','stop');
-    }); 
 });
 
+var lastPressure, lastTemperature;
+function getPressure() {
+	var query = 'select * from pressure group by * order by desc limit 1';
+	dbClient.query(query, function (err, result) {
+        result = result[0][0];
+        var data =
+        {
+            cid: result.ChairUUID, 
+            time: result.time,
+            p: {}
+        }
+        for(var i = 0; i < 10; i++) {
+            data.p[i] = result[i];
+        }
+        
+        lastPressure = data;
+        
+    })
+    return lastPressure;
+}
+
+function getTemperature() {
+	
+	var query = 'select * from temperature group by * order by desc limit 1';
+	dbClient.query(query, function (err, result) {
+		console.log(result);
+        result = result[0][0];
+        var data =
+        {
+            cid: result.ChairUUID, 
+            time: result.time,
+            t: {}
+        }
+        for(var i = 0; i < 1; i++) {
+            data.t[i] = result[i];
+        }
+        
+        lastTemperature = data;
+        
+    })
+    return lastTemperature;
+}
+
+/* generate testdata, if no db is available */
 function generatePressure() {
 	var pressure = { p: {} };
 	for(var i = 0; i < 10; i++) {
