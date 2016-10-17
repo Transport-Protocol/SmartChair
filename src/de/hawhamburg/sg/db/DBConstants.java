d031ef0f707b00e7ef3d04e0eb9b8ce81b5e6af2
michel <michel.kirstein@haw-hamburg.de>
2016-10-17 10:29:26 +0200

    MQ-Access for DBconnector

--- src/de/hawhamburg/sg/db/DBConstants.java
+++ src/de/hawhamburg/sg/db/DBConstants.java
@@ -3,17 +3,17 @@ package de.hawhamburg.sg.db;
 public final class DBConstants {
 	private DBConstants(){throw new RuntimeException();}
 
-	public static final String PROPERTIES_FILENAME="mwrp.properties";
+	public static final String PROPERTIES_FILENAME="db.properties";
 
 	public static final String MQ_HOST_KEY="mqHost";
 	public static final String MQ_PORT_KEY="mqPort";
 	public static final String MQ_USER_KEY="mqUser";
 	public static final String MQ_PASSW_KEY="mqPassw";
 
-	public static final String DB_HOST_KEY="mqHost";
-	public static final String DB_PORT_KEY="mqPort";
-	public static final String DB_USER_KEY="mqUser";
-	public static final String DB_PASSW_KEY="mqPassw";
+	public static final String DB_HOST_KEY="dbHost";
+	public static final String DB_PORT_KEY="dbPort";
+	public static final String DB_USER_KEY="dbUser";
+	public static final String DB_PASSW_KEY="dbPassw";
 	public static final String DB_NAME_KEY="dbName";
 	
 	public static final String DEFVAL_MQ_HOST="localhost";
