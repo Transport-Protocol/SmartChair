8809e5605d06a4298a68cd1bc4ccb6fdbd936710
Fabian Schmidt <fabian.schmidt@haw-hamburg.de>
2016-10-19 12:41:54 +0200

    Added commandline arguments in DebugDataGenerator

--- src/com/hawhamburg/sg/debug/DebugDataGenerator.java
+++ src/com/hawhamburg/sg/debug/DebugDataGenerator.java
@@ -16,15 +16,67 @@ import com.rabbitmq.client.Channel;
 import com.rabbitmq.client.Connection;
 import com.rabbitmq.client.ConnectionFactory;
 
+import de.hawhamburg.sg.db.DBConnector;
+import de.hawhamburg.sg.db.DBProperties;
 import com.hawhamburg.sg.mwrp.RabbitMqConstants;
 
 public class DebugDataGenerator {
 	
 	private static final int MAXVALUESPERDATA = 10;
-	private static final int NUMDATA = 10;
+	private static final int STANDARDNUMDATA = 10;
+	private static final int STANDARDDELAY = 1;
 	private static int valueId = 0;
 	
 	public static void main(String[] args) 
+	{
+		String modus = null;
+		int delay = STANDARDDELAY;
+		int numData = STANDARDNUMDATA;
+		for(int i = 0; i< args.length; i++)
+		{
+			if(args[i].equals("raspi") || args[i].equals("server") || args[i].equals("database"))
+			{
+				modus = args[i];
+			}
+			else if (args[i].equals(delay)) 
+			{
+				delay = Integer.parseInt(args[i+1]);
+			}
+			else if(args[i].equals("numData"))
+			{
+				numData = Integer.parseInt(args[i+1]);
+			}
+			else if(args[i].equals("help"))
+			{
+				System.out.println("Usage: java Myprogram MODUS [Options] || java Myprogram [Options] MODUS");
+				System.out.println("MODUS: \n"
+						+ "raspi			Push messages to the raspi queue(has to run on raspi)\n"
+						+ "server			Push messages to the server queue(has to run on server)\n"
+						+ "database			Push messages to the database(Configuration in properties file)");
+				System.out.println("Options:\n"
+						+ "delay NUMBER		Set the delay between the messages in ms\n"
+						+ "numData NUMBER	Set the count of messages\n");
+				System.exit(0);
+			}
+			
+		}
+		
+		if(modus.equals("raspi"))
+		{
+			testRasPi(numData, delay);
+		}
+		else if(modus.equals("server"))
+		{
+			testServer(numData, delay);
+		}
+		else if(modus.equals("database"))
+		{
+			testDatabase(numData, delay);
+		}
+		
+	}
+	
+	public static void testRasPi(int numData, int delay)
 	{
 		ConnectionFactory factory = new ConnectionFactory();		
 		
@@ -32,22 +84,66 @@ public class DebugDataGenerator {
 			Connection conn = factory.newConnection();
 			Channel channel = conn.createChannel();
 			
-			for(int i=0; i<NUMDATA; i++)
+			for(int i=0; i<numData; i++)
 			{
-				channel.basicPublish(RabbitMqConstants.MQ1_EXCHANGE_NAME, RabbitMqConstants.MQ1_ROUTING_KEY, null, getExampleData().getBytes());
+				channel.basicPublish(RabbitMqConstants.MQ1_EXCHANGE_NAME, RabbitMqConstants.MQ1_ROUTING_KEY, null, getSensorMessageAsJson().getBytes());
+				Thread.sleep(delay);
 			}
 			
 			channel.close();
 			conn.close();
 			
-		} catch (IOException | TimeoutException e1) {
+		} catch (IOException | TimeoutException | InterruptedException e1) {
 			// TODO Auto-generated catch block
 			e1.printStackTrace();
 		}
 		
 	}
 	
-	public static String getExampleData()
+	public static void testServer(int numData, int delay)
+	{
+		ConnectionFactory factory = new ConnectionFactory();		
+		
+		try {
+			Connection conn = factory.newConnection();
+			Channel channel = conn.createChannel();
+			
+			for(int i=0; i<numData; i++)
+			{
+				channel.basicPublish(RabbitMqConstants.MQ2_EXCHANGE_NAME, RabbitMqConstants.MQ2_ROUTING_KEY, null, getChairMessageAsJson().getBytes());
+				Thread.sleep(delay);
+			}
+			
+			channel.close();
+			conn.close();
+			
+		} catch (IOException | TimeoutException | InterruptedException e1) {
+			// TODO Auto-generated catch block
+			e1.printStackTrace();
+		}
+	}
+	
+	public static void testDatabase(int numData, int delay)
+	{
+		DBProperties props = null;
+		try {
+			props = new DBProperties(false);
+		    DBConnector connector = new DBConnector(props);
+		    
+		    for(int i = 0; i<numData; i++)
+		    {
+		    	connector.write(getChairMessage());
+				Thread.sleep(delay);
+		    }
+		    
+		} catch (IOException | InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	    
+	}
+	
+	public static String getSensorMessageAsJson()
 	{
 		Random rng=new Random();
 		ObjectMapper objectMapper = new ObjectMapper();
@@ -63,6 +159,20 @@ public class DebugDataGenerator {
 		String json = "";
 		try {
 			json = objectMapper.writeValueAsString(msg);
+		} catch (JsonProcessingException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		return json;
+	}
+	
+	public static String getChairMessageAsJson()
+	{
+		
+		ObjectMapper objectMapper = new ObjectMapper();
+		String json = "";
+		try {
+			json = objectMapper.writeValueAsString(getChairMessage());
 		} catch (JsonProcessingException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
