253deae486e86b67283048e396bdff353e3ffc1a
nico <nico@nico-linux.fritz.box>
2016-10-26 17:02:45 +0200

    Kinda broken triangulation with 2 beacons

--- middleware/src/com/hawhamburg/sg/mwrp/gui/view/LocationView.java
+++ middleware/src/com/hawhamburg/sg/mwrp/gui/view/LocationView.java
@@ -20,6 +20,13 @@ import com.hawhamburg.sg.mwrp.DataProvider;
 import com.hawhamburg.sg.mwrp.gui.MwrpCanvas;
 
 public class LocationView implements IView {
+	private static final int MIN_DB_VALUE=25;
+	private static final int MAX_DB_VALUE=95;
+	private static final int MAX_DB_DIFF=MAX_DB_VALUE-MIN_DB_VALUE;
+	
+	private static final int FRAME_X=5;
+	private static final int FRAME_Y=60;
+	
 	private final MwrpCanvas canvas;
 	private Image backButton;
 	private final DataProvider dataProvider;
@@ -40,6 +47,13 @@ public class LocationView implements IView {
 		g.drawImage(backButton, 5, 5, 45, 45, 0, 0, 40, 40, null);
 		g.setStroke(new BasicStroke(2));
 		g.setColor(Color.black);
+
+		g.drawRect(FRAME_X, FRAME_Y, width-FRAME_X*2, height-FRAME_Y-5);
+		
+		g.drawString("1", FRAME_X+2, FRAME_Y+fm.getHeight());
+		g.drawString("2", width-FRAME_X-fm.stringWidth("2")-2, FRAME_Y+fm.getHeight());
+		g.drawString("3", FRAME_X+2, height-7);
+		g.drawString("4", width-FRAME_X-fm.stringWidth("4")-2, (height-7));
 		
 		LocationValue[] vals=dataProvider.getMostRecentLcoation();
 		if(vals==null)
@@ -50,8 +64,21 @@ public class LocationView implements IView {
 		}
 		for(int i=0;i<vals.length;i++)
 		{
-			g.drawString("M"+(i+1)+": "+vals[i].getdB()+"dB", 5, 65+(fm.getHeight()+5)*i);
+			g.drawString("M"+(i+1)+": "+vals[i].getdB()+"dB", 50+(i/2*70), 5+fm.getHeight()+(fm.getHeight()+5)*(i%2));
 		}
+		int[] distance=new int[vals.length];
+		for(int i=0;i<distance.length;i++)
+		{
+			distance[i]=Math.abs(vals[i].getdB()+MIN_DB_VALUE);
+		}
+		
+		double a=(Math.pow(distance[0],2)-Math.pow(distance[1],2)+Math.pow(MAX_DB_DIFF,2))/(2*MAX_DB_DIFF);
+		double h =Math.sqrt(Math.pow(distance[0], 2)-Math.pow(a, 2));
+		double pxy=MAX_DB_DIFF*(a/MAX_DB_DIFF);
+		double x=pxy+h;
+		double y=pxy-h;
+		g.drawString("x: "+x, 190, 5+fm.getHeight());
+		g.drawString("y: "+y, 190, 5+fm.getHeight()*2);
 	}
 
 	@Override
