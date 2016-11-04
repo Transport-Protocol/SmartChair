df9b9aa0b21d83143ec79944ce252c24be6b7022
hauke.buhr <hauke.buhr@haw-hamburg.de>
2016-11-04 10:13:18 +0100

    added time interval as programm argument.

--- sensor/pi_python/main.py
+++ sensor/pi_python/main.py
@@ -8,8 +8,7 @@ import HAL
 import Threads
 
 # Define here all supported sensors.
-sensors = ["pressure"]
-sensor = ["temperature", "location", "acceleration"]
+sensor = ["pressure", "temperature", "acceleration", "distance"]
 
 parser = argparse.ArgumentParser()
 
@@ -21,44 +20,38 @@ parser.add_argument("--socket",
                     action="store_true", dest="socket", default=False,
                     help="This flag causes json strings to send by socket.")
 
-for entry in sensors:
-    parser.add_argument("--" + entry,
-                        action="store_true", dest=entry, default=False,
-                        help="This flag enables " + entry + " sensors in test mode")
-
 for entry in sensor:
-    parser.add_argument("--" + entry,
-                        action="store_true", dest=entry, default=False,
-                        help="This flag enables " + entry + " sensor in test mode")
+    parser.add_argument("--" + entry, type=float,
+                        help="This flag enables " + entry + " sensor. You can enter an interval after the flag."
+                                                            " Interval in Seconds! example = 1.0")
 
 args = parser.parse_args()
 
 # get collect all needed sensor threads
 json_queue = queue.Queue()
 threads = []
-if args.pressure or args.temperature or args.acceleration:
-    print("Test mode active!")
 
-    if args.pressure:
-        print("pressure active!")
-        threads.append(Threads.SensorEvaluator(1, "SensorEvaluator_pressure", 1, 0.50, json_queue, HAL.pressure))
+if args.pressure is not None:
+    print("pressure active!")
+    threads.append(
+        Threads.SensorEvaluator(1, "SensorEvaluator_pressure", 1, args.pressure, json_queue, HAL.pressure))
 
-    if args.temperature:
-        print("temperature active!")
-        threads.append(
-            Threads.SensorEvaluator(2, "SensorEvaluator_temperature", 2, 5, json_queue, HAL.temperature))
+if args.temperature is not None:
+    print("temperature active!")
+    threads.append(
+        Threads.SensorEvaluator(2, "SensorEvaluator_temperature", 2, args.temperature, json_queue, HAL.temperature))
 
-    if args.acceleration:
-        print("acceleration active!")
-        threads.append(
-            Threads.SensorEvaluator(3, "SensorEvaluator_accelerator", 3, 1, json_queue, HAL.acceleration_sensor))
+if args.acceleration is not None:
+    print("acceleration active!")
+    threads.append(
+        Threads.SensorEvaluator(3, "SensorEvaluator_accelerator", 3, args.acceleration, json_queue, HAL.acceleration_sensor))
 
-else:
-    threads.append(Threads.SensorEvaluator(1, "SensorEvaluator_pressure", 1, 0.50, json_queue, HAL.pressure))
-    threads.append(Threads.SensorEvaluator(2, "SensorEvaluator_temperature", 2, 30, json_queue, HAL.temperature))
-    threads.append(Threads.SensorEvaluator(3, "SensorEvaluator_accelerator", 3, 1, json_queue, HAL.acceleration_sensor))
+if args.distance is not None:
+    print("distance active!")
+    threads.append(
+        Threads.SensorEvaluator(3, "SensorEvaluator_distance", 4, args.distance, json_queue, HAL.acceleration_sensor))
 
-# get right communication medium
+# acquire right communication medium
 if args.rabbit:
     print("rabbit active!")
     communicator = communicator = Communicators.RabbitMQCommunicator("127.0.0.1", "sg.ex.sensor_values",
@@ -70,7 +63,6 @@ elif args.socket:
 else:
     communicator = Communicators.CommunicatorDummy()
 
-communicator.setup_connection()
 threads.append(Threads.MQCommunicator(10, "MQ_Communicator", 10, json_queue, communicator))
 
 # start the communicator thread
